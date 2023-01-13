3e94994a8e5ed49b340a297626d54d7cfcce4534
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2023-01-13 15:47:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Its a config file for initial setup/startup of the Robot
+# @Author      : Sukarna Jana
+# @Version     : 0.1V
+# @Last Update : 21-Nov-2022
+# @License     : (Copyleft) GNU
+######################################################
+#[Setup for main.py]
+#[Setup for Speaking]
+speechRate = 140
+speechVolume = 1.0
+speechGender = 1#2
